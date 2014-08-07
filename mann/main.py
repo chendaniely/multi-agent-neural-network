0abe39fd9f35f8494fd314da5b501d9a0c6ae8fb
Daniel Chen <Maveri2k@yahoo.com>
2014-08-07 13:08:02 -0400

    model run to create network, agents, and agent-network

--- mann/main.py
+++ mann/main.py
@@ -11,7 +11,15 @@ def main():
     # probablity for edge creation [0, 1]
     p = 0.1
 
+    # Create Erdos-Renyi graph
+    my_network = network.DirectedFastGNPRandomGraph(n, p)
+    print(my_network.G.edges()) # edge list
+    print(my_network.G.edges_iter())
+    my_network.show_graph()
     
+    network_of_agents = network_agent.NetworkAgent()
+    network_of_agents.create_multidigraph_of_agents_from_edge_list(n, my_network.G.edges_iter())
+
 
 if __name__ == "__main__":
     print("Running")
