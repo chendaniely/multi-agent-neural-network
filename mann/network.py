7083f057cdd950df4a7418dd9feefa23266bb055
Daniel Chen <Maveri2k@yahoo.com>
2014-08-07 13:07:21 -0400

    creates the DirectedFastGNPRandomGraph

---
+++ mann/network.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Network(object):
+    def __init__(self):
+        pass
+
+    def get_edge_list(self):
+        pass
+
+
+class DirectedFastGNPRandomGraph(nx.MultiDiGraph):
+    def __init__(self, n, p):
+        self.G = nx.fast_gnp_random_graph(n, p, directed=True)
+
+    def show_graph(self):
+        nx.draw_circular(self.G)
+        plt.show()
+
