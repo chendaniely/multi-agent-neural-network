aa50c0f576636c5543f70250f42f561f0f48baa8
Daniel Chen <Maveri2k@yahoo.com>
2014-08-08 14:09:07 -0400

    random select and update code moved to function to use in step()

--- mann/main.py
+++ mann/main.py
@@ -3,6 +3,23 @@
 import network
 import network_agent
 
+def random_select_and_update(network_of_agents):
+    n = len(network_of_agents.G)
+    # randomly select nodes from network_of_agents
+    num_update = n // 10 # select 10% of the nodes for update, performs floor division
+    agents_for_update = network_of_agents.sample_network(num_update)
+    print(agents_for_update)
+    
+    print(network_of_agents.G.nodes()[agents_for_update[0].get_key()])
+
+    # update agents who were selected
+    for selected_agent in agents_for_update:
+        print("updating: ", network_of_agents.G.nodes()[selected_agent.get_key()])
+        print('pre-update binary_state', selected_agent.binary_state)
+        selected_agent.update_agent_binary_state()
+        print('post-update_agent_binary_state', selected_agent.binary_state)
+
+
 def step(network_of_agents):
     random_select_and_update(network_of_agents)
 
@@ -27,12 +44,6 @@ def main():
     # predecessors are agents who influence the current agent
     network_of_agents.set_predecessors_for_each_node()
 
-    # randomly select nodes from network_of_agents
-    s = n // 10 # select 10% of the nodes for update, performs floor division
-    agents_for_update = network_of_agents.sample_network(s)
-    print(agents_for_update)
-    
-    print(network_of_agents.G.nodes()[agents_for_update[0].get_key()])
     # randomly select nodes from network_of_agents to seed
     num_seed = 1
     agents_to_seed = network_of_agents.sample_network(num_seed)
