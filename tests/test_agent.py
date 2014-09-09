6ced6f0e853fc10c70535307293768b73a1ea378
Daniel Chen <Maveri2k@yahoo.com>
2014-09-09 00:13:04 -0400

    removed old/commented-out unit tests, but keep the unit test notes

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -6,23 +6,6 @@ import io
 from mann import agent
 
 
-# def setup_lens_agent():
-#     print('Setting up LENS agent')
-#     test_lens_agent = agent.LensAgent(10)
-
-
-# @with_setup(setup_lens_agent)
-# def test_write_agent_state_to_ex():
-#     test_file = 'write_agent_state_to_ex.test'
-#     assert(1 == 3, 'you failed')
-#     assert(3 == 3, 'you no fail')
-
-
-# # @with_setup(setup_lens_agent)
-# def test_pass():
-#     assert(1 == 1, 'this should not fail')
-
-
 #################
 # Unit Test notes
 #################
