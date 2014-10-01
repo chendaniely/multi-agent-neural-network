db6943af3bb29a64976c91241fe9dbb6ceb97f90
Daniel Chen <Maveri2k@yahoo.com>
2014-10-01 11:19:26 -0400

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name='mann',
+    packages=['mann'],  # this must be the same as the name above
+    version='0.1',
+    description='Agent-based model framework for learning',
+    author='Daniel Chen',
+    author_email='',
+    # use the URL to the github repo
+    url='https://github.com/chendaniely/multi-agent-neural-network',
+    # I'll explain this in a second
+    download_url='https://github.com/chendaniely/multi-agent-neural-network/\
+                  tarball/0.1',
+    # arbitrary keywords
+    keywords=['agent-based model', 'abm', 'neural network', 'learning',
+              'diffusion'],
+    classifiers=[]
+)
