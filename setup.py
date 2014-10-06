87713a12e040e90728d9bd715da102512750ebd7
Daniel Chen <Maveri2k@yahoo.com>
2014-10-06 15:17:18 -0400

    delete comments

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ setup(
     description='Agent-based model framework for learning',
     author='Daniel Chen',
     author_email='chend@vbi.vt.edu',
-    # use the URL to the github repo
     url='https://github.com/chendaniely/multi-agent-neural-network',
-    # I'll explain this in a second
     download_url='https://github.com/chendaniely/multi-agent-neural-network/\
                   tarball/0.0.1',
-    # arbitrary keywords
     keywords=['agent-based model', 'abm', 'neural network', 'learning',
               'diffusion'],
     classifiers=[],
