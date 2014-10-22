dc73b797c249f3c51467ae1edb922d17cbd601d7
Daniel Chen <Maveri2k@yahoo.com>
2014-10-21 20:24:45 -0400

    funciton to return a list of lists used to write sit for weight train

--- mann/agent.py
+++ mann/agent.py
@@ -465,3 +465,25 @@ class LensAgent(Agent):
         else:
             raise ValueError('Number to flip not 0 or 1')
 
+    def _create_weight_training_examples(self, filename,
+                                         base_example,
+                                         num_train_examples,
+                                         num_train_mutations):
+        list_of_example_values = []
+        open(filename, 'w').close()
+        for train_example in range(num_train_examples):
+            # train_list = []
+            train_list = base_example[:]
+            random_idx = random.randint(0, len(train_list) - 1)
+            # print('random int: ', random_idx, file=sys.stderr)
+            # print('train list pre : ', train_list, file=sys.stderr)
+            new_value = self._flip_1_0_value(train_list[random_idx])
+            # print('new value:', new_value, file=sys.stderr)
+            # print('list idx value: ', train_list[random_idx],
+            #       file=sys.stderr)
+            train_list[random_idx] = new_value
+            # print('train list post: ', train_list, file=sys.stderr)
+            assert train_list is not base_example, 'lists are equal'
+            list_of_example_values.append(train_list)
+        return list_of_example_values
+
