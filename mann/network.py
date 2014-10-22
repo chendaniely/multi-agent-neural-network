2e36927cfc57690dd22022dd9846b13c62a15997
Daniel Chen <Maveri2k@yahoo.com>
2014-10-21 20:16:09 -0400

    more understandable param name

--- mann/network.py
+++ mann/network.py
@@ -16,7 +16,7 @@ class DirectedFastGNPRandomGraph(nx.MultiDiGraph):
     def __init__(self, n, p):
         self.G = nx.fast_gnp_random_graph(n, p, directed=True)
 
-    def show_graph(self, name):
+    def show_graph(self, path_and_name):
         nx.draw_circular(self.G)
         # plt.show()
-        plt.savefig(name)
+        plt.savefig(path_and_name)
