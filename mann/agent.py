33d1eec0b5ee2b5ba44d368a31ace2f36ec3a86e
Daniel Chen <Maveri2k@yahoo.com>
2014-10-22 22:17:46 -0400

    new .out file line numbers for AutoEncoderArch

--- mann/agent.py
+++ mann/agent.py
@@ -325,7 +325,8 @@ class LensAgent(Agent):
         # enter the actual file line numbers
         # the 1 offset is used in the actual fxn call
         # f is the .out file to be read
-        return tuple([122, 126, 128, 132])
+        # TODO pass these values in from config file
+        return tuple([5, 9, 10, 14])
 
     def _get_new_state_values_from_out_file(self, file_dir):
         list_of_new_state = []
