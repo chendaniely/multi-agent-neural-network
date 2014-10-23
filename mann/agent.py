373c63b91372282dffe47179b256f09832e77004
Daniel Chen <Maveri2k@yahoo.com>
2014-10-22 22:19:09 -0400

    get base example from config file

--- mann/agent.py
+++ mann/agent.py
@@ -307,6 +307,8 @@ class LensAgent(Agent):
         # print('a environment: ', lens_env.get('a'))
         # print('a environment: ', lens_env.get('a'), file=sys.stderr)
 
+        base_example = self._str_to_int_list(base_example)
+
 
         # list of 'words' passed into the subprocess call
         lens_weight_command = ['lens', ' -nogui',  weight_in_file]
