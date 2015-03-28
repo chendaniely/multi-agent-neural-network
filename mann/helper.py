4ec78a32d5a26b4feca570c3dbe52fe1f4d4ee7c
Daniel Chen <Maveri2k@yahoo.com>
2015-03-28 01:58:49 -0400

    convert_str_to_int_array string parameter cannot be empty string

--- mann/helper.py
+++ mann/helper.py
@@ -6,9 +6,13 @@ def convert_str_to_int_array(string, delim=','):
     :parm string: delimited string of ints
     :type string: str
 
+    .. note::
+    The string parameter cannot be empty
+
     :parm delim: string delimited, default is ',' (a comma)
     :type delim: numpy.ndarray
     """
     assert isinstance(string, str), "string parameter passed is not type str"
     assert isinstance(delim, str), "delim parameter passed is not type str"
+    assert string != '', 'string parameter is empty'
     return np.array([int(s) for s in string.strip().split(delim)])
