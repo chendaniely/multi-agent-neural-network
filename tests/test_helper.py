789012f4a63fe2095b034528cd9e6d28251d4482
Daniel Chen <Maveri2k@yahoo.com>
2015-03-28 03:19:01 -0400

    unittest helper.mutate

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python
+import random
+# import sys
+
 import numpy as np
 
 from mann import helper
@@ -21,3 +24,18 @@ def test_flip_1_0():
     calculated = helper.flip_1_0(0)
     expected = 1
     assert calculated == expected
+
+def test_mutate():
+    calculated = helper.mutate([0, 0, 0, 0], 0)
+    expected = [0, 0, 0, 0]
+    assert np.array_equal(np.array(calculated), np.array(expected))
+
+    calculated = helper.mutate([0, 0, 0, 0], 1)
+    expected = [1, 1, 1, 1]
+    assert np.array_equal(np.array(calculated), np.array(expected))
+
+    random.seed(1)
+    calculated = helper.mutate([0, 0, 0, 0], .5)
+    # print(calculated, file=sys.stderr)
+    expected = [1, 0, 0, 1]
+    assert np.array_equal(np.array(calculated), np.array(expected))
