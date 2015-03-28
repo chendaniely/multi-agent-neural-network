823874ce711c3ea65d2d21b4b2a097620b4406c6
Daniel Chen <Maveri2k@yahoo.com>
2015-03-28 02:21:46 -0400

    unittest test_flip_1_0

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -7,3 +7,12 @@ def test_convert_str_to_int_list():
     converted = helper.convert_str_to_int_array('1, 2, 3')
     expected = np.array([1, 2, 3])
     assert np.array_equal(converted, expected)
+
+def test_flip_1_0():
+    calculated = helper.flip_1_0(1)
+    expected = 0
+    assert calculated == expected
+
+    calculated = helper.flip_1_0(0)
+    expected = 1
+    assert calculated == expected
