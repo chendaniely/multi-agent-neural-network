9ccb8ccc1e3388c91d2b83cfe98f38c9e3ab2e3b
Daniel Chen <Maveri2k@yahoo.com>
2015-04-02 08:39:20 -0400

    made test_small_recurrent_attitude_in_string variable to be used for expected

--- tests/test_lens_in_writer.py
+++ tests/test_lens_in_writer.py
@@ -5,17 +5,17 @@ import sys
 from mann import helper
 from mann import lens_in_writer
 
+test_small_recurrent_attitude_in_string = """name: agent_state
+I: 1 2 3;
+name: infl1
+I: 4 5 6;
+"""
 
 def test_generate_lens_recurrent_attitude():
     test_writer = lens_in_writer.LensInWriterHelper()
     calculated = test_writer.generate_lens_recurrent_attitude(
         '1, 2, 3', '4, 5, 6')
-    expected =\
-    """name: agent_state
-I: 1 2 3;
-name: infl1
-I: 4 5 6;
-"""
+    expected = test_small_recurrent_attitude_in_string
     # print(calculated, file=sys.stderr)
     # print(expected, file=sys.stderr)
     assert calculated == expected
@@ -26,12 +26,7 @@ def test_generate_lens_recurrent_attitude_list():
     array_to_string_infl = helper.convert_list_to_delim_str([4, 5, 6], ' ')
     calculated = test_writer.generate_lens_recurrent_attitude(
         array_to_string_agent, array_to_string_infl)
-    expected =\
-    """name: agent_state
-I: 1 2 3;
-name: infl1
-I: 4 5 6;
-"""
+    expected = test_small_recurrent_attitude_in_string
     # print(calculated, file=sys.stderr)
     # print(expected, file=sys.stderr)
     assert calculated == expected
