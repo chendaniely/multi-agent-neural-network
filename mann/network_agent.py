f6ec1e4d6b2f71984a57d6df2557e28b372b72be
Daniel Chen <Maveri2k@yahoo.com>
2015-04-02 15:47:29 -0400

    bugfix: write_edge_list takes a graph, a graph does not have this fxn as a method

--- mann/network_agent.py
+++ mann/network_agent.py
@@ -77,7 +77,7 @@ class NetworkAgent(object):
         return self.G
 
     def export_edge_list(self, export_file_dir, **kwargs):
-        self.G.write_edgelist(export_file_dir, **kwargs)
+        nx.write_edgelist(self.G, export_file_dir, **kwargs)
 
     def set_predecessors_for_each_node(self):
         # iterate through all nodes in network
