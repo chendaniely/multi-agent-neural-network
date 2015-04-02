26d99a5d015e90eeda8a3d0e1c89e8e6e9223339
Daniel Chen <Maveri2k@yahoo.com>
2015-04-02 08:37:52 -0400

    return float of delim list instead of int

--- mann/helper.py
+++ mann/helper.py
@@ -18,7 +18,7 @@ def convert_str_to_int_array(string, delim=','):
     assert isinstance(string, str), "string parameter passed is not type str"
     assert isinstance(delim, str), "delim parameter passed is not type str"
     assert string != '', 'string parameter is empty'
-    return np.array([int(s) for s in string.strip().split(delim)])
+    return np.array([float(s) for s in string.strip().split(delim)])
 
 def convert_list_to_delim_str(list_to_convert, delim=','):
     """Return a string delimited by delim from a list
