6beaa8e9e019ee25d5b58eb06a6133c0054fe8a8
Daniel Chen <Maveri2k@yahoo.com>
2015-04-02 08:38:19 -0400

    rename unittest function to denote array, instead of list

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from mann import helper
 
-def test_convert_str_to_int_list():
+def test_convert_str_to_int_array():
     converted = helper.convert_str_to_int_array('1, 2, 3')
     expected = np.array([1, 2, 3])
     assert np.array_equal(converted, expected)
